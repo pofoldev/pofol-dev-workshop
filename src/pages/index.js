ea269be0790386fb3c5e538300d4d51aae8a76a6
lexifdev <sl@lxf.kr>
2020-01-28 18:48:20 +0900

    index에 gitter link추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default () => (
             <ul>
                 <li>
                     <Link to="/tutorials/">튜토리얼</Link>
+                    <a href="https://gitter.im/pofol-dev/community">채팅방 (부담 없이 누구나 질문 가능합니다)</a>
                 </li>
             </ul>
         </div>
